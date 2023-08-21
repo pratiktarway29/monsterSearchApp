13a56af7876d87395c50d5cb9e7c179b135bd0c9
pratiktarway29 <twexaa@gmail.com>
2023-08-21 17:29:41 +0530

    Worked on CSS

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,10 +2,12 @@ import Card from '../card/card.component';
 import './card-list.styles.css';
 
 const CardList = ({ monsters }) => (
-    <div className='card-list'>
-        {monsters.map(monster => {
-            return <Card monster={monster} key={monster.id} />;
-        })}
+    <div className='card-list-container'>
+        <div className='card-list'>
+            {monsters.map(monster => {
+                return <Card monster={monster} key={monster.id} />;
+            })}
+        </div>
     </div>
 );
 
